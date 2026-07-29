dad4f5628729e5bd5562b2f50955b7189bf17ee6
Subham Saha <subhamranjansaha006@gmail.com>
2026-07-30 03:20:30 +0530

    Added Stat page functionality

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,7 +1,37 @@
-
+import {useEffect,useState} from 'react'
 function Stats()
 {
-    return <h1>Statistics</h1>
+    const [predictions,setPredictions]=useState([]);
+    useEffect(()=>{
+        const savedPredictions=localStorage.getItem("predictions")
+    
+    if(savedPredictions)
+    {
+        setPredictions(JSON.parse(savedPredictions))
+    }},[])
+    const attempted=predictions.filter(p=>p.actualOutcome).length
+    const correct=predictions.filter(p=>p.isCorrect).length
+
+    return (
+        <div>
+            <p>{correct} out of {attempted} predictions correct so far!</p>
+            {predictions.map((prediction, index)=>
+            
+                (prediction.actualOutcome && (
+                    <div key={prediction.id}>
+                        <p>{index+1}</p>
+                        <p>{prediction.sport}</p>
+                        <p>{prediction.matchLabel}</p>
+                        <p>{prediction.predictedOutcome}</p>
+                        <p>{prediction.actualOutcome}</p>
+                        <p>{prediction.isCorrect ? '✅ Correct' : '❌ Wrong'}</p>
+                    </div>
+                    
+    
+                ))
+            )}
+        </div>
+    )
 }
 
 export default Stats
