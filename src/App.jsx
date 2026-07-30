13fe01c988bc7d09a1daa51b74fd0165792f32b3
Subham Saha <subhamranjansaha006@gmail.com>
2026-07-31 02:40:15 +0530

    Added styling of links and formatting of landing/Home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App()
 {
     return (
         <div className="min-h-screen bg-gradient-to-br from-cyan-400 to-yellow-300">
-            <nav>
-                <Link to='/Home'>Home</Link>
-                <Link to='/Cricket'>Cricket</Link>
-                <Link to='/Football'>Football</Link>
-                <Link to='/Stats'>Stats</Link>
+            <nav className="flex items-center justify-center gap-20 px-20 py-4">
+                <Link to='/Home' className="px-7 py-3 bg-gray-300 rounded-lg font-medium text-2xl hover:bg-[#c4c9d1] transition-colors hover:scale-110 transition all">Home</Link>
+                <Link to='/Cricket' className="px-7 py-3 bg-gray-300 rounded-lg font-medium text-2xl hover:bg-[#c4c9d1] transition-colors hover:scale-110 transition all">Cricket</Link>
+                <Link to='/Football' className="px-7 py-3 bg-gray-300 rounded-lg font-medium text-2xl hover:bg-[#c4c9d1] transition-colors hover:scale-110 transition all">Football</Link>
+                <Link to='/Stats' className="px-7 py-3 bg-gray-300 rounded-lg font-medium text-2xl hover:bg-[#c4c9d1] transition-colors hover:scale-110 transition all">Stats</Link>
             </nav>
 
             <Routes>
